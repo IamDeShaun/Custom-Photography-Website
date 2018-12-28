229546ed1835b396a9685476b4e51c03f9b80cf0
DeShaun Jones <ceo@deshaunjones.com>
2018-12-27 23:44:05 -0500

    Cleaning Up
    Cleaning up code

--- js/main.js
+++ js/main.js
@@ -1,31 +1,5 @@
 $(function() {
-        
-    // Cache the Window object
-    var $window = $(window);
-    
-    // Parallax Backgrounds
-    // Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
-    
-    $('section[data-type="background"]').each(function(){
-      var $bgobj = $(this); // assigning the object
-      
-      $(window).scroll(function() {
-      
-        // Scroll the background at var speed
-        // the yPos is a negative value because we're scrolling it UP!								
-        var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-        
-        // Put together our final background position
-        var coords = '50% '+ yPos + 'px';
-        
-        // Move the background
-        $bgobj.css({ backgroundPosition: coords });
-        
-      }); // end window scroll
-    });
-    
-  });
-  
+
       var $backToTop = $(".back-to-top");
       $backToTop.hide();
   
@@ -39,4 +13,6 @@ $(function() {
   
       $backToTop.on('click', function(e) {
         $("html, body").animate({scrollTop: 0}, 500);
-      });
+      })
+      
+    });
